92103eab57e0becb99ec4bec4e4eb689f7be407a
Vladislav Kolesnikov <forshortmrmeth@gmail.com>
2025-11-06 16:41:12 +0300

    add simple server

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
