1c89a0f0382ebda3c4c5aca5a4765bedae394469
Vladislav Kolesnikov <forshortmrmeth@gmail.com>
2025-11-06 17:00:09 +0300

    add index.html, file handler

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import "net/http"
 func main() {
 	mux := http.NewServeMux()
 
+	mux.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("."))))
+
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
